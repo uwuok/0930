7bb9f0ebd9f326b3e62be4fdc912e2ad4972a77f
uwuok <natsumi7129453@gmail.com>
2024-09-30 20:27:42 +0800

    update

--- bmi.py
+++ bmi.py
@@ -1,4 +1,4 @@
-weight = float(input('Please input your weight'))
-height = float(input('Please input your height'))
+weight = float(input('Please input your weight:'))
+height = float(input('Please input your height:'))
 bmi = weight / ((height / 100) ** 2)
-print(bmi)
+print(f'Your BMI is:{bmi:.2f}')
